c47966f5a0e3013e6b781eef20aa3b45b7b1ae1c
liammccall <liamkm895@gmail.com>
2024-10-28 17:52:01 -0400

    this is better

--- real-time-fluid-simulation/backend/fluid.py
+++
@@ -1,8 +0,0 @@
-class Fluid:
-    
-    density = 0
-    viscosity = 0
-    
-    def __init__(self, density, viscosity):
-        self.density = density
-        self.viscosity = viscosity
--- real-time-fluid-simulation/backend/main.py
+++
@@ -1,7 +0,0 @@
-# Just as an entry point not really gonna use this
-
-def main():
-    pass
-    
-if(__name__ == "__main__"):
-    main()
